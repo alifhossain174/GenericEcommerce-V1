ad292ac3c7fdb928c068b0726abc67c0ec314a46
Md Fahim Hossain <alifhossain174@gmail.com>
2024-01-07 22:21:25 +0600

    Updating Code

--- generic_commerce_v1.sql
+++ generic_commerce_v1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 07, 2024 at 05:19 PM
+-- Generation Time: Jan 07, 2024 at 05:20 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
