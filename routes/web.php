e184fe577d3c4c7d8493c048bd78c28eb4d0190e
Md Fahim Hossain <alifhossain174@gmail.com>
2024-01-16 13:19:09 +0600

    Updating Code

--- routes/web.php
+++ routes/web.php
@@ -362,6 +362,8 @@ Route::group(['middleware' => ['auth', 'CheckUserType']], function () {
     Route::get('/view/delivery/charges', [DeliveryChargeController::class, 'viewAllDeliveryCharges'])->name('ViewAllDeliveryCharges');
     Route::get('/get/delivery/charge/{id}', [DeliveryChargeController::class, 'getDeliveryCharge'])->name('GetDeliveryCharge');
     Route::post('/update/delivery/charge', [DeliveryChargeController::class, 'updateDeliveryCharge'])->name('UpdateDeliveryCharge');
+
+    // upazaila thana
     Route::get('view/upazila/thana', [DeliveryChargeController::class, 'viewUpazilaThana'])->name('ViewUpazilaThana');
     Route::get('get/upazila/info/{id}', [DeliveryChargeController::class, 'getUpazilaInfo'])->name('getUpazilaInfo');
     Route::post('update/upazila/info', [DeliveryChargeController::class, 'updateUpazilaInfo'])->name('UpdateUpazilaInfo');
