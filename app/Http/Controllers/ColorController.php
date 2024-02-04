3ca0ae4ca4c9eea3bdf85d2d273b211b4be6cbb8
Md Fahim Hossain <alifhossain174@gmail.com>
2024-02-04 10:28:21 +0600

    Updating Code

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Color;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use DataTables;
+use Yajra\DataTables\DataTables;
 
 class ColorController extends Controller
 {
