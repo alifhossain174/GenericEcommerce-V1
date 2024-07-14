2ed256664436419c6f1ba24d8a4e277dcbcc60ed
Fahim <alifhossain174@gmail.com>
2024-07-14 14:00:30 +0600

    Updating COde

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -388,10 +388,11 @@ class OrderController extends Controller
         $shippingInfo = ShippingInfo::where('order_id', $order->id)->first();
         $billingAddress = BillingAddress::where('order_id', $order->id)->first();
         $orderDetails = DB::table('order_details')
+                            ->leftJoin('stores', 'order_details.store_id', 'stores.id')
                             ->leftJoin('products', 'order_details.product_id', 'products.id')
                             ->leftJoin('categories', 'products.category_id', 'categories.id')
                             ->leftJoin('units', 'order_details.unit_id', 'units.id')
-                            ->select('order_details.*', 'products.name as product_name', 'units.name as unit_name', 'categories.name as category_name')
+                            ->select('order_details.*', 'stores.store_name', 'products.name as product_name', 'units.name as unit_name', 'categories.name as category_name')
                             ->where('order_id', $order->id)
                             ->get();
         $generalInfo = DB::table('general_infos')->select('logo', 'logo_dark', 'company_name')->first();
